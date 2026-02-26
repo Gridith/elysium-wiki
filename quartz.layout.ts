175904253b4ba4b0a152814c88fea620ab6dd3b5
Gridith <rickdeboer2001@outlook.com>
2026-02-26 19:55:18 +0100

    Quartz sync: Feb 26, 2026, 7:55 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,7 +17,6 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
