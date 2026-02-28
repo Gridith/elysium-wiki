72d95d8e9e108d0942e487e10c1d1a0b6a2ac1e6
Gridith <rickdeboer2001@outlook.com>
2026-02-28 16:54:33 +0100

    Quartz sync: Feb 28, 2026, 4:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "gridith.github.io/elysium-wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
